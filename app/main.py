08a75a4361b57e069ef5b75df6376f5e7f122854
Luiz Fellype <lfcassago@gmail.com>
2021-07-01 22:12:53 -0300

    search and show persons details by the name

--- app/main.py
+++ app/main.py
@@ -1,8 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request
   
 app = Flask(__name__)
   
+people = [
+        { 
+                "name": "Luiz"
+        }
+]
+
 @app.route("/")
+@app.route('/index')
 def home_view():
-        return "<h1>Hello world !!!</h1>"
+        return render_template('index.html')
+
+
+@app.route("/<name>")
+def person_detail(name):
+        person = {}
+        for p in people:
+                if p["name"] == name:
+                        person = p
+                
+        return render_template('index.html', person=person)
+
+
+@app.route('/task', methods=['POST'])
+def add_task():
+        name = request.form['content']
+
+        return redirect(f'/{name}')
+
+
+
 
